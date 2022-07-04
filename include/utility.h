4ebb580747e4d08a760dc80c9a249f2250e0caca
mhahn <mhahn@student.42heilbronn.de>
2022-07-04 14:20:21 +0200

    Fully implemented pair

--- include/utility.h
+++ include/utility.h
@@ -7,8 +7,7 @@
 
 namespace ft {
 
-    template<class T1,
-            class T2>
+    template<class T1, class T2>
     struct pair {
         T1 first;
         T2 second;
@@ -24,10 +23,39 @@ namespace ft {
 
     };
 
-    template<class T1,
-            class T2>
+    template<class T1, class T2>
     pair<T1, T2> make_pair(T1 t, T2 u) {
         return pair<T1, T2>(t, u);
+    }
+
+    template <class T1, class T2>
+    bool operator==(const pair<T1, T2> & lhs, const pair<T1, T2> & rhs) {
+        return lhs.first == rhs.first && lhs.second = rhs.second;
+    }
+
+    template <class T1, class T2>
+    bool operator!=(const pair<T1, T2> & lhs, const pair<T1, T2> & rhs) {
+        return !(lhs == rhs);
+    }
+
+    template <class T1, class T2>
+    bool operator<(const pair<T1, T2> & lhs, const pair<T1, T2> & rhs) {
+        return lhs.first < rhs.first || lhs.second < rhs.second;
+    }
+
+    template <class T1, class T2>
+    bool operator<=(const pair<T1, T2> & lhs, const pair<T1, T2> & rhs) {
+        return !(rhs < lhs);
+    }
+
+    template <class T1, class T2>
+    bool operator>(const pair<T1, T2> & lhs, const pair<T1, T2> & rhs) {
+        return rhs < lhs;
+    }
+
+    template <class T1, class T2>
+    bool operator>=(const pair<T1, T2> & lhs, const pair<T1, T2> & rhs) {
+        return !(lhs < rhs);
     }
 }
 
