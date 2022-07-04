5fa130ac1aa9f6860a47a004e20637e8bc5e782b
mhahn <mhahn@student.42heilbronn.de>
2022-07-04 14:33:51 +0200

    Added documentation to the pair

--- include/utility.h
+++ include/utility.h
@@ -7,23 +7,54 @@
 
 namespace ft {
 
-    template<class T1, class T2>
+    /**
+     * A struct to combine two objects of any type.
+     *
+     * @tparam T1 The type of the first element
+     * @tparam T2 The type of the second element
+     * @since 04.07.22
+     */
+    template <class T1, class T2>
     struct pair {
+        /** The first object. */
         T1 first;
+        /** The second object. */
         T2 second;
 
+        /**
+         * Creates a pair. The two bound objects are initialized using their default constructor.
+         */
         pair() : first(), second() {}
 
-        pair(const T1 &x, const T2 &y) : first(x), second(y) {}
+        /**
+         * Creates a pair. The two bound objects are initialized using the given values.
+         *
+         * @param x The object to initialize the first object with.
+         * @param y The object to initialize the second object with.
+         */
+        pair(const T1 & x, const T2 & y) : first(x), second(y) {}
 
-        template<class U1,
-                class U2>
-        pair(const pair<U1, U2> &p): first(p.first), second(p.second) {}
-
-
+        /**
+         * Copies the given pair.
+         *
+         * @tparam U1 The type of the first parameter to copy
+         * @tparam U2 The type of the second parameter to copy
+         * @param p The pair which should be copied.
+         */
+        template <class U1, class U2>
+        pair(const pair<U1, U2> & p): first(p.first), second(p.second) {}
     };
 
-    template<class T1, class T2>
+    /**
+     * Constructs a pair using the given objects.
+     *
+     * @tparam T1 The type of the first object.
+     * @tparam T2 The type of the second object.
+     * @param t The first object.
+     * @param u The second object.
+     * @return A newly created pair with the given objects.
+     */
+    template <class T1, class T2>
     pair<T1, T2> make_pair(T1 t, T2 u) {
         return pair<T1, T2>(t, u);
     }
