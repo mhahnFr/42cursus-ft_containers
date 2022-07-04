2263f35c2498729632c5d56779ac86c6316bb296
mhahn <mhahn@student.42heilbronn.de>
2022-07-04 13:27:08 +0200

    Added correct namespace, added make_pair

--- include/utility.h
+++ include/utility.h
@@ -5,18 +5,30 @@
 #ifndef FT_CONTAINERS_UTILITY_H
 #define FT_CONTAINERS_UTILITY_H
 
-template <class T1,
-          class T2>
-struct pair {
-    T1 first;
-    T2 second;
+namespace ft {
 
-    pair(): first(), second() {}
-    pair(const T1 & x, const T2 & y): first(x), second(y) {}
+    template<class T1,
+            class T2>
+    struct pair {
+        T1 first;
+        T2 second;
 
-    template <class U1,
-              class U2>
-    pair(const pair<U1, U2> & p): first(p.first), second(p.second) {}
-};
+        pair() : first(), second() {}
+
+        pair(const T1 &x, const T2 &y) : first(x), second(y) {}
+
+        template<class U1,
+                class U2>
+        pair(const pair<U1, U2> &p): first(p.first), second(p.second) {}
+
+
+    };
+
+    template<class T1,
+            class T2>
+    pair<T1, T2> make_pair(T1 t, T2 u) {
+        return pair<T1, T2>(t, u);
+    }
+}
 
 #endif //FT_CONTAINERS_UTILITY_H
