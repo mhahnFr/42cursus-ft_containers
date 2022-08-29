578612a55b589f81672b5277231a44d25d97e2f2
mhahn <mhahn@student.42heilbronn.de>
2022-08-29 16:36:08 +0200

    Added iterator_traits

---
+++ include/iterator.hpp
@@ -0,0 +1,45 @@
+//
+// Created by Manuel Hahn on 8/29/22.
+//
+
+#ifndef FT_CONTAINERS_ITERATOR_HPP
+#define FT_CONTAINERS_ITERATOR_HPP
+
+#include <cstddef>
+
+namespace ft {
+    struct input_iterator_tag  {};
+    struct output_iterator_tag {};
+    struct forward_iterator_tag:       public input_iterator_tag         {};
+    struct bidirectional_iterator_tag: public forward_iterator_tag       {};
+    struct random_access_iterator_tag: public bidirectional_iterator_tag {};
+
+    template<class Iter>
+    struct iterator_traits {
+        typedef typename Iter::difference_type   difference_type;
+        typedef typename Iter::value_type        value_type;
+        typedef typename Iter::pointer           pointer;
+        typedef typename Iter::reference         reference;
+        typedef typename Iter::iterator_category iterator_category;
+    };
+
+    template<class T>
+    struct iterator_traits<T *> {
+        typedef std::ptrdiff_t             difference_type;
+        typedef T                          value_type;
+        typedef T *                        pointer;
+        typedef T &                        reference;
+        typedef random_access_iterator_tag iterator_category;
+    };
+
+    template<class T>
+    struct iterator_traits<const T *> {
+        typedef std::ptrdiff_t             difference_type;
+        typedef T                          value_type;
+        typedef const T *                  pointer;
+        typedef const T &                  reference;
+        typedef random_access_iterator_tag iterator_category;
+    };
+}
+
+#endif //FT_CONTAINERS_ITERATOR_HPP
