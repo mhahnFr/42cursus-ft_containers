694967a4f46582a04d7a8e1680631e891ce5d19c
mhahn <mhahn@student.42heilbronn.de>
2022-09-30 18:11:38 +0200

    Adapted old main to the new map

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 	#include <vector>
 	namespace ft = std;
 #else
-//	#include <map.h>
+	#include <map.hpp>
 	#include <stack.hpp>
 	#include <vector.hpp>
 #endif
@@ -60,7 +60,7 @@ int main(int argc, char** argv) {
 	ft::stack<int> stack_int;
 	ft::vector<Buffer> vector_buffer;
 	ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
-//	ft::map<int, int> map_int;
+	ft::map<int, int> map_int;
 
 	for (int i = 0; i < COUNT; i++)
 	{
@@ -90,19 +90,19 @@ int main(int argc, char** argv) {
 	
 	for (int i = 0; i < COUNT; ++i)
 	{
-//		map_int.insert(ft::make_pair(rand(), rand()));
+		map_int.insert(ft::make_pair(rand(), rand()));
 	}
 
 	int sum = 0;
 	for (int i = 0; i < 10000; i++)
 	{
-//		int access = rand();
-//		sum += map_int[access];
+		int access = rand();
+		sum += map_int[access];
 	}
 	std::cout << "should be constant with the same seed: " << sum << std::endl;
 
 	{
-//		ft::map<int, int> copy = map_int;
+		ft::map<int, int> copy = map_int;
 	}
 	MutantStack<char> iterable_stack;
 	for (char letter = 'a'; letter <= 'z'; letter++)
